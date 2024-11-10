b3e14eaca11470b8cf16836f771219b01cb1d782
StoneHee99 <kiit0901@gmail.com>
2024-11-10 20:21:24 +0900

    init: initial project

---
+++ src/main/java/com/hidden_treasure/HiddenTreasureApplication.java
@@ -0,0 +1,13 @@
+package com.hidden_treasure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiddenTreasureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiddenTreasureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hidden_treasure/HiddenTreasureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hidden_treasure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiddenTreasureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
